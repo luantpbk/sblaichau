9398608ab7167dc909c3c60b10c24dcc7c2f5df4
luantpbk <thanhluanbka@gmail.com>
2026-07-17 15:32:07 +0700

    Fix syntax error inside Layout.jsx template literal HTML strings by replacing JSX onClick with global HTML window events

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -727,14 +727,14 @@ export default function Layout({ children }) {
 				<div class="elementor-element elementor-element-278e0172 elementor-view-framed elementor-shape-square elementor-widget elementor-widget-icon" data-id="278e0172" data-element_type="widget" data-widget_type="icon.default">
 				<div class="elementor-widget-container">
 					<div class="elementor-icon-wrapper">
-			<a class="elementor-icon" href="#" onClick={(e) => { e.preventDefault(); setIsZaloOpen(true); }} target="_blank">
+			<a class="elementor-icon" href="#" onclick="event.preventDefault(); window.dispatchEvent(new Event('openZaloPopup'));" target="_blank">
 			<svg aria-hidden="true" class="e-font-icon-svg e-fab-Zalo" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>			</a>
 		</div>
 				</div>
 				</div>
 				<div class="elementor-element elementor-element-721dc762 elementor-widget elementor-widget-heading" data-id="721dc762" data-element_type="widget" data-widget_type="heading.default">
 				<div class="elementor-widget-container">
-			<h2 class="elementor-heading-title elementor-size-default"><a href="#" onClick={(e) => { e.preventDefault(); setIsZaloOpen(true); }}>Zalo</a></h2>		</div>
+			<h2 class="elementor-heading-title elementor-size-default"><a href="#" onclick="event.preventDefault(); window.dispatchEvent(new Event('openZaloPopup'));">Zalo</a></h2>		</div>
 				</div>
 					</div>
 				</div>
@@ -743,14 +743,14 @@ export default function Layout({ children }) {
 				<div class="elementor-element elementor-element-1eff31b7 elementor-view-framed elementor-shape-square elementor-widget elementor-widget-icon" data-id="1eff31b7" data-element_type="widget" data-widget_type="icon.default">
 				<div class="elementor-widget-container">
 					<div class="elementor-icon-wrapper">
-			<a class="elementor-icon" href="#" onClick={(e) => { e.preventDefault(); setIsContactOpen(true); }}>
+			<a class="elementor-icon" href="#" onclick="event.preventDefault(); window.dispatchEvent(new Event('openContactPopup'));">
 			<svg aria-hidden="true" class="e-font-icon-svg e-far-envelope" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>			</a>
 		</div>
 				</div>
 				</div>
 				<div class="elementor-element elementor-element-1786e4b4 elementor-widget elementor-widget-heading" data-id="1786e4b4" data-element_type="widget" data-widget_type="heading.default">
 				<div class="elementor-widget-container">
-			<h2 class="elementor-heading-title elementor-size-default"><a href="#" onClick={(e) => { e.preventDefault(); setIsContactOpen(true); }}>Liên hệ</a></h2>		</div>
+			<h2 class="elementor-heading-title elementor-size-default"><a href="#" onclick="event.preventDefault(); window.dispatchEvent(new Event('openContactPopup'));">Liên hệ</a></h2>		</div>
 				</div>
 					</div>
 				</div>
@@ -804,14 +804,14 @@ export default function Layout({ children }) {
 				<div class="elementor-element elementor-element-267d74cf elementor-view-default elementor-widget elementor-widget-icon" data-id="267d74cf" data-element_type="widget" data-widget_type="icon.default">
 				<div class="elementor-widget-container">
 					<div class="elementor-icon-wrapper">
-			<a class="elementor-icon" href="#" onClick={(e) => { e.preventDefault(); setIsContactOpen(true); }}>
+			<a class="elementor-icon" href="#" onclick="event.preventDefault(); window.dispatchEvent(new Event('openContactPopup'));">
 			<svg aria-hidden="true" class="e-font-icon-svg e-fas-envelope" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg>			</a>
 		</div>
 				</div>
 				</div>
 				<div class="elementor-element elementor-element-16e7530b elementor-widget elementor-widget-heading" data-id="16e7530b" data-element_type="widget" data-widget_type="heading.default">
 				<div class="elementor-widget-container">
-			<h2 class="elementor-heading-title elementor-size-default"><a href="#" onClick={(e) => { e.preventDefault(); setIsContactOpen(true); }}>Email</a></h2>		</div>
+			<h2 class="elementor-heading-title elementor-size-default"><a href="#" onclick="event.preventDefault(); window.dispatchEvent(new Event('openContactPopup'));">Email</a></h2>		</div>
 				</div>
 				</div>
 		<div class="elementor-element elementor-element-35a5028 e-con-full e-flex e-con e-child" data-id="35a5028" data-element_type="container">
